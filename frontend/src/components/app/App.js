84b6646b096a231e43659ad1cbcabeab9f10bffd
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-16 14:57:22 +0000

    Readded Navbar elements to App.js

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import LoginForm from '../auth/LoginForm'
 import SignUpForm from '../user/SignUpForm'
 import React, { useState } from 'react';
+import Navbar from '../navbar/Navbar'
 import Feed from '../feed/Feed'
 import {
   useNavigate,
@@ -10,13 +11,18 @@ import {
 } from "react-router-dom";
 
 const App = () => {
-    return (
-      <Routes>
-        <Route path='/posts'  element={<Feed navigate={ useNavigate() }/>}/>
-        <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
-        <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
-      </Routes>
-    );
+  return (
+    <div className='app'>
+      <Navbar />
+      <div className='pages'>
+        <Routes>
+          <Route path='/posts'  element={<Feed navigate={ useNavigate() }/>}/>
+          <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
+          <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
