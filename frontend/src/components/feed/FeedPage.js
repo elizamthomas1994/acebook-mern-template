77633706e59099b51a9f735480497ac2abdd4178
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2023-01-20 10:32:58 +0000

    menu no longer shows on profile page

--- frontend/src/components/feed/FeedPage.js
+++ frontend/src/components/feed/FeedPage.js
@@ -9,30 +9,32 @@ import Feed from './Feed';
 import { useNavigate } from 'react-router';
 
 const FeedPage = ({ navigate }) => {
-  // const [posts, setPosts] = useState([]);
-  // const [token, setToken] = useState(window.localStorage.getItem('token'));
-  // const [postAdded, setPostAdded] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [token, setToken] = useState(window.localStorage.getItem('token'));
+  const [postAdded, setPostAdded] = useState(false);
   const userId = window.localStorage.getItem('user_id');
 
-  // useEffect(() => {
-  //   if (token) {
-  //     fetch('/posts', {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     })
-  //       .then((response) => response.json())
-  //       .then(async (data) => {
-  //         window.localStorage.setItem('token', data.token);
-  //         setToken(window.localStorage.getItem('token'));
-  //         setPosts(data.posts.reverse());
-  //         setPostAdded(false);
-  //         // console.log(data.posts);
-  //       });
-  //   }
-  //   // eslint-disable-next-line
-  // }, [postAdded]);
+  useEffect(() => {
+    if (token) {
+      fetch('/posts', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then(async (data) => {
+          window.localStorage.setItem('token', data.token);
+          setToken(window.localStorage.getItem('token'));
+          setPosts(data.posts.reverse());
+          setPostAdded(false);
+          // console.log(data.posts);
+        });
+    }
+    // eslint-disable-next-line
+  }, [postAdded]);
 
+
+  if (token) {
     return (
      
       <div className="feed-page">
@@ -40,6 +42,9 @@ const FeedPage = ({ navigate }) => {
           <Feed />
         </div>
     );
+  } else {
+        navigate('/login');
+  }
   } 
 
 export default FeedPage;
