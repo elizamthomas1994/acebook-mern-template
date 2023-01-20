d3ce322c2969c9182204674a84b591375ff84cc2
henrychow <113641610+henrow5@users.noreply.github.com>
2023-01-20 12:43:49 +0000

    add avatars-pics

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import profile_placeholder from './profile_placeholder.jpeg';
 import Card from '../Helpers/Card.js';
 import './profile.css';
-import Feed from '../feed/Feed'
+import Feed from '../feed/Feed';
 import { useParams } from 'react-router-dom';
 import coverPhoto from './cover-photo.jpg';
 import '../../index.css';
 
 const Profile = () => {
-
   const { id } = useParams();
   const [token, setToken] = useState(window.localStorage.getItem('token'));
   const [user, setUser] = useState([]);
@@ -36,9 +35,7 @@ const Profile = () => {
     // eslint-disable-next-line
   }, []);
 
-
   const profileMatch = user._id === userId;
-
 
   const handleAddFriend = () => {
     // Pushes Eliza into Bill's friends list:
@@ -57,85 +54,99 @@ const Profile = () => {
     //   },
     //   body: JSON.stringify({ post_id: post._id, user_id: userId }),
     // }).then(() => setPostAdded(true));
-  }
+  };
 
-return(
-  <div>
-    <div className='profile-header'>
-      <div className='coverPhoto'>
+  return (
+    <div>
+      <div className="profile-header">
+        <div className="coverPhoto"></div>
+        <div></div>
       </div>
-      <div>
 
+      <div className="profile">
+        <div className="left">
+          <div className="user-info">
+            <Card>
+              {/* <img src={ user.avatar } /> */}
+              <div className="profile-pic-container">
+                <img
+                  className="profile-pic"
+                  src={user.avatar}
+                  alt={'profile pic'}
+                />
+              </div>
+              <h1>{user.name}</h1>
+              <div className="user-details">
+                <p>{user.hometown}</p>
+                <p>{user.profession}</p>
+                <p>{user.relationship_status}</p>
+              </div>
+
+              <div>
+                {profileMatch ? (
+                  <div className="edit-button">
+                    <button>Edit profile</button>
+                  </div>
+                ) : (
+                  <div className="friend-button">
+                    <button onClick={handleAddFriend}>Add Friend</button>
+                  </div>
+                )}
+              </div>
+            </Card>
+          </div>
+
+          <div className="intro">
+            <Card>
+              <h3>Intro</h3>
+              <p>{user.bio}</p>
+            </Card>
+          </div>
+
+          <div className="photos">
+            <Card>
+              <div className="photo">
+                <Card>
+                  <img
+                    src={profile_placeholder}
+                    style={{ width: '50px', height: '50px' }}
+                  />
+                </Card>
+              </div>
+              <div className="photo">
+                <Card>
+                  <img
+                    src={profile_placeholder}
+                    style={{ width: '50px', height: '50px' }}
+                  />
+                </Card>
+              </div>
+              <div className="photo">
+                <Card>
+                  <img
+                    src={profile_placeholder}
+                    style={{ width: '50px', height: '50px' }}
+                  />
+                </Card>
+              </div>
+              <div className="photo">
+                <Card>
+                  <img
+                    src={profile_placeholder}
+                    style={{ width: '50px', height: '50px' }}
+                  />
+                </Card>
+              </div>
+            </Card>
+          </div>
+        </div>
+
+        <div className="posts right">
+          <Feed />
+        </div>
       </div>
     </div>
+  );
+};
 
-    <div className='profile'>
-      <div className='left'>
-        <div className='user-info'>
-          <Card>
-            <img src={ user.avatar } />
-            <h1>{ user.name }</h1>
-            <div className="user-details">
-              <p>{user.hometown}</p>
-              <p>{user.profession}</p>
-              <p>{user.relationship_status}</p>
-            </div>
-
-            <div>
-              {profileMatch ? (
-              <div className='edit-button'>
-                <button>Edit profile</button>
-              </div>
-              ) : (
-              <div className='friend-button'>
-                <button onClick={ handleAddFriend }>Add Friend</button>
-              </div>
-              )}
-            </div>
-            </Card>
-        </div>
-
-        <div className='intro'>
-          <Card> 
-            <h3>Intro</h3>
-            <p>{user.bio}</p>
-          </Card>   
-   
-        </div>
-
-        <div className='photos'>
-          <Card>
-            <div className='photo'>
-              <Card>
-                <img src={profile_placeholder} style={{width: "50px", height: "50px"}} />
-              </Card>
-            </div>
-            <div className='photo'>
-              <Card>
-                <img src={profile_placeholder} style={{width: "50px", height: "50px"}} />
-              </Card>
-            </div>
-            <div className='photo'>
-              <Card>
-                <img src={profile_placeholder} style={{width: "50px", height: "50px"}} />
-              </Card>
-            </div>
-            <div className='photo'>
-              <Card>
-                <img src={profile_placeholder} style={{width: "50px", height: "50px"}} />
-              </Card>
-            </div>
-          </Card>
-        </div>
-      </div>
-
-                
-      <div className='posts right'>
-        <Feed />
-      </div>
-    </div>  
-  </div>
- )
-}
-
-export default Profile
+export default Profile;
